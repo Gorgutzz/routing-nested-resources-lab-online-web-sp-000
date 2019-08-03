2ed85caffa59050b048abec7e2acebd748bad856
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-08-03 23:01:27 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   resources :songs
 
   resources :artists do
-      resources :songs, only: [:index, :show]
+    resources :songs, only: [:index, :show]
   end
 end
